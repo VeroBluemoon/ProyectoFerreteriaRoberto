aa97b3cc28d7d7c088f1a040e7fca3c5c2f29dbc
Veronica Apollonio <93684626+VeroBluemoon@users.noreply.github.com>
2022-03-07 23:11:47 +0000

    Agrego Sorage y JSON

--- script.js
+++ script.js
@@ -178,3 +178,126 @@ let inputColor = document.getElementById("inputColor")
 inputColor.addEventListener('input', (e) => {
     console.log(e.target.value)
 })
+
+class Producto {
+    constructor(nombre, marca, precio, stock) {
+    this.nombre = nombre;
+    this.marca = marca;
+    this.precio = precio;
+    this.stock = stock;
+    }
+}
+
+let formProducto = document.getElementById('formProducto')
+let botonProductos = document.getElementById('botonProductos')
+let divProductos = document.getElementById('divProductos')
+
+let productos = []
+// Eventos
+
+formProducto.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let nombre = document.getElementById('nombre').value
+    let marca = document.getElementById('marca').value
+    let precio = document.getElementById('precio').value
+    let stock = document.getElementById('stock').value
+
+    const producto = new Producto(nombre, marca, precio, stock)
+    productos.push(producto)
+
+    console.log(productos)
+    formProducto.reset()
+})
+
+botonProducto.addEventListener("click", () => {
+    if(productos.length !== 0) {
+        divProductos.innerHTML = ""
+        productos.forEach((producto , indice) => {
+            divProductos.innerHTML += 
+            `
+            <div class="card" id="producto${indice} style="width: 18rem;">
+             <div class="card-body">
+              <h5 class="card-title">${producto.nombre}</h5>
+                 <p class="card-text">Marca: ${producto.marca}</p>
+                 <p class="card-text">Precio: ${producto.precio}</p>
+                 <p class="card-text">Stock: ${producto.stock}</p>
+                 <button class="btn btn-secondary">Eliminar</button>
+             </div>
+            </div>
+             `
+     
+         })  
+    } else {
+        divProductos.innerHTML = "No se agrego ningun producto"
+
+    }
+    
+})
+
+localStorage.setItem('Saludar', 'Hola')
+localStorage.setItem('Saludar', 'Adios')
+
+let valorLocalStorage = localStorage.getItem('Saludar')
+
+console.log(valorLocalStorage)
+
+for(let i = 0; i < localStorage.length; i ++) {
+    let clave = localStorage.key(i)
+    console.log(clave)
+    console.log(localStorage.getItem(clave))
+}
+
+localStorage.removeItem('PersonasT')
+//localStorage.clear()
+//sessionStorage.setItem('Saludar', 'Hola')
+
+class Personal {
+    constructor(nombre, apellido, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+}
+
+const personal1 = new Personal("Martin", "Figueredo", 22)
+const personal2 = new Personal("Lucia", "Fortuna", 25)
+
+let personales = [personal1,personal2]
+//let personaJSON = JSON.stringify(personas)
+localStorage.setItem('Personales', JSON.stringify(personales))
+
+let personalesParseadas = JSON.parse(localStorage.getItem('Personales'))
+console.log(personalesParseadas)
+
+/*let arrayPersonas = []
+
+localStorage.setItem('Personas', JASON.stringify(arrayPersonas))
+let formPersona = document.getElementById('formPersona')
+
+formPersona.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let datForm = new FormData(e.target)
+    //console.log(datForm.get('nombre')) //document.getElementById('idnombre').
+    const persona = new Persona(datForm.get('nombre'), datForm.get('apellido'),
+    arrayPersonas.push(persona)
+    localStorage.setItem('Personas', JSON.stringify(arrayPersonas))
+    formPersona.reset()
+    //Recordar adaptar el form en html
+})*/
+
+let botonPersonal = document.getElementById('botonPersonal')
+let divPersonal = document.getElementById('divPersonal')
+
+botonPersonal.addEventListener("click", () => {
+    let personalesParseadas = JSON.parse(localStorage.getItem('Personales'))
+    personalesParseadas.forEach(personales => {   
+     divPersonal.innerHTML += 
+            `
+                 <p> ${personales.nombre} </p>
+                 <p> ${personales.apellido} </p>
+                 <p> ${personales.edad} </p>
+                 
+             `
+    })
+})
